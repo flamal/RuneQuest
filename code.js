675c9571d35fcf63835f8856b9b9389c89ee9895
flamal <mikko.siivinen@gmail.com>
2023-05-01 01:43:45 +0300

    sheet-values

--- code.js
+++ code.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const str = document.getElementById("str");
-  const con = document.getElementById("con");
-  const dex = document.getElementById("dex");
-  const siz = document.getElementById("siz");
-  const int = document.getElementById("int");
-  const pow = document.getElementById("pow");
-  const cha = document.getElementById("cha");
-  const hitPoints = document.getElementById("hitPoints");
-  
-  document.addEventListener("input", function() {
-    const strValue = parseInt(str.value) || 0;
-    const conValue = parseInt(con.value) || 0;
-    hitPoints.textContent = strValue + conValue;
-  });
+    const str = document.getElementById("str");
+    const con = document.getElementById("con");
+    const dex = document.getElementById("dex");
+    const siz = document.getElementById("siz");
+    const int = document.getElementById("int");
+    const pow = document.getElementById("pow");
+    const cha = document.getElementById("cha");
+    const hitPoints = document.getElementById("hitPoints");
+    
+    document.addEventListener("input", function() {
+        const strValue = parseInt(str.value) || 0;
+        const conValue = parseInt(con.value) || 0;
+        const dexValue = parseInt(dex.value) || 0;
+        const sizValue = parseInt(siz.value) || 0;
+        const intValue = parseInt(int.value) || 0;
+        const powValue = parseInt(pow.value) || 0;
+        const chaValue = parseInt(cha.value) || 0;
+        hitPoints.textContent = strValue + sizValue;
+        fatiguePoints.textContent = strValue + conValue;
+        magicPoints.textContent = powValue;
+        knowledge.textContent = intValue;
+    });
 });
